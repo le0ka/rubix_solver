92995d16ae58b96fa267f0b12ed6f51056cafb18
Leo Kaiser <leohkaiser@gmail.com>
2023-08-21 22:17:01 -0500

    graph

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # rubix_solver
 Rubix cube solver based on algorithm I use
+
+Option #1:
+
+Class: Square -> color + edges
+
+Class cube: matrix of cubes -> horizontal turn, vertical turn, face turn
+
+Option #2:
+
+Map of cubes
+
+Option #3:
+
+Matrix of cubes
--- rubix_cube_solver/cube.py
+++ rubix_cube_solver/cube.py
@@ -1,5 +1,18 @@
-import numpy as np
+import random
 
-class square:
-    def __init__(self, color) -> None:
+class cube:
+    def __init__(self, color, conn):
         self.color = color
+        conn = []
+    def add(self, cube):
+        self.conn.append(cube)
+
+def rand_color():
+    colors = ['w', 'b', 'y', 'g', 'o', 'y']
+    return colors[random.randint(len(colors))]
+
+#Start from top left corner, work across and down
+def generate_cube():
+     curr = 1
+     while curr < 26:
+         
